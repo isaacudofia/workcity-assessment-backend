7f05306bfd0a0ea6f2abe53d227157097fef56fd
Isaac Udofia <isaacudofia0@gmail.com>
2025-07-28 01:30:37 +0100

    Added CORS to the server.js for cors broswer origin

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 // Routes
 app.use("/api/auth", require("./routes/auth"));
